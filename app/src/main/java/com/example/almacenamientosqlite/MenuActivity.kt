9f6dadf1e37a5646e93b3eb33788c55a699d06c0
JesusLopez28 <jesus.lopez280402@gmail.com>
2024-11-10 13:37:31 -0600

    Initial commit

--- app/src/main/java/com/example/almacenamientosqlite/MenuActivity.kt
+++ app/src/main/java/com/example/almacenamientosqlite/MenuActivity.kt
@@ -1,20 +1,70 @@
 package com.example.almacenamientosqlite
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.FragmentTransaction
 
 class MenuActivity : AppCompatActivity() {
+
+    private lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_menu)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Configurar la Toolbar
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar) // Esto configura la Toolbar como la ActionBar
+
+        // Cargar el fragmento por defecto
+        //loadFragment(ParquesFragment())
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflar el menú de opciones en la Toolbar
+        menuInflater.inflate(R.menu.menu_options, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Manejar las opciones del menú
+        return when (item.itemId) {
+            R.id.menu_parques -> {
+                //loadFragment(ParquesFragment())
+                true
+            }
+
+            R.id.menu_listado_parques -> {
+                //loadFragment(ListadoParquesFragment())
+                true
+            }
+
+            R.id.menu_guardabosques -> {
+                //loadFragment(GuardabosquesFragment())
+                true
+            }
+
+            R.id.menu_listado_guardabosques -> {
+                //loadFragment(ListadoGuardabosquesFragment())
+                true
+            }
+
+            R.id.menu_cerrar_sesion -> {
+                finish() // Cierra la actividad y vuelve a la pantalla de login
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun loadFragment(fragment: androidx.fragment.app.Fragment) {
+        // Cargar el fragmento seleccionado en el FrameLayout
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frameLayout, fragment)
+        transaction.commit()
     }
 }
